8b3791d86a94376cfee0dafd9097039493ebd64a
tujianglin <450287957@qq.com>
2022-04-11 08:37:38 +0800

    初始化

---
+++ packages/utils/index.ts
@@ -0,0 +1,15 @@
+/* 
+ * @Description: 普通方法
+ * @FilePath: /tjl-sdk/packages/utils/index.ts
+ */
+
+/**
+ * @description: 两数字相加
+ * @author: tujianglin
+ * @param n1: 数字1
+ * @param n2: 数字2
+ * @return: Number
+ * @example 
+ * add(1, 2) => 3
+ */
+export const add = (n1: number, n2: number): number => n1 + n2
