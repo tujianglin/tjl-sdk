3232bd1f2732daa3e8daffd541c271e32292cf52
涂江林 <jianglin.tu@wimisoft.com>
2022-04-25 13:13:58 +0800

    feat: eslint

--- packages/file/index.ts
+++ packages/file/index.ts
@@ -1,4 +1,4 @@
-/* 
+/*
  * @Description: 文档方法
  * @FilePath: /tjl-sdk/packages/utils/file.ts
  */
@@ -12,28 +12,30 @@
  * previewFile({url: 'http://xxx.xxx/xx.txt', type: 'txt'})
  */
 export const previewFile = ({ url, type = 'txt' }: { url: string; type: string }): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    let req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.responseType = 'blob';
-    req.send();
-    req.onload = () => {
-      const blob = req.response;
-      var reader = new FileReader();
-      reader.readAsArrayBuffer(blob);
-      reader.onload = (loadEvent) => {
-        const buffer = loadEvent.target.result;
-        switch (type) {
-          case 'txt': {
-            const txtReader = new FileReader();
-            txtReader.onload = (txtEvent) => resolve(txtEvent.target.result);
-            txtReader.onerror = (err) => reject(err);
-            txtReader.readAsText(new Blob([buffer]), 'utf-8');
-          }
-          default:
-            break;
-        }
-      };
-    };
-  });
-};
+	return new Promise((resolve, reject) => {
+		const req = new XMLHttpRequest()
+		req.open('GET', url)
+		req.responseType = 'blob'
+		req.send()
+		req.onload = () => {
+			const blob = req.response
+			const reader = new FileReader()
+			reader.readAsArrayBuffer(blob)
+			reader.onload = loadEvent => {
+				const buffer = loadEvent.target.result
+				switch (type) {
+					case 'txt': {
+						const txtReader = new FileReader()
+						txtReader.onload = txtEvent => resolve(txtEvent.target.result)
+						txtReader.onerror = err => reject(err)
+						txtReader.readAsText(new Blob([buffer]), 'utf-8')
+						break
+					}
+					default: {
+						break
+					}
+				}
+			}
+		}
+	})
+}
